e2d5ef99fe92a001fff991a3d9b1a5514f669f85
Cyril CHAPON <cyril@permettezmoideconstruire.fr>
2020-07-13 09:08:37 +0200

    Add timestamp helpers

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,2 +1,5 @@
 export const eurosToCents = (eurs: number): number => Math.round(eurs * 100)
 export const centsToEurs = (cents: number): number => cents / 100
+
+export const almaTimestampToDate = (almaTimestamp: number): Date => new Date(almaTimestamp * 1000)
+export const dateToAlmaTimestamp = (date: Date): number => Math.round(date.getTime() / 1000)
